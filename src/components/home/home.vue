d930e415f2a7852f88c75fa0c23f7666a7aa17f3
demoThink <demotest123@163.com>
2018-05-23 11:01:22 +0800

    'contain布局home组件展示页面'

--- src/components/home/home.vue
+++ src/components/home/home.vue
@@ -1,15 +1,51 @@
 <template>
-  <div>
-    <p>home component</p>
+  <div class="container">
+    <el-container class="el-container">
+      <el-header class="header">
+        <el-row :gutter="20">
+          <el-col :span="9">
+            <div class="grid-content bg-purple">
+              <img src="./logo-bai.png" alt="">
+            </div>
+          </el-col>
+          <el-col :span="12">
+            <div class="grid-content bg-purple">电商后天管理系统</div>
+          </el-col>
+          <el-col :span="2">
+            <div class="grid-content bg-purple">
+              <a href="#">退出</a>
+            </div>
+          </el-col>
+        </el-row>
+      </el-header>
+      <el-container>
+        <el-aside class="aside">Aside</el-aside>
+        <el-main class="main">Main</el-main>
+      </el-container>
+    </el-container>
   </div>
 </template>
 
 <script>
-  export default {
-    
-  }
+export default {};
 </script>
 
-<style scoped>
-
+<style>
+.container,
+.el-container {
+  height: 100%;
+}
+.header {
+  line-height: 60px;
+  background-color: #b3c0d1;
+}
+.aside {
+  width: 200px;
+  height: 100%;
+  background-color: #d3dce6;
+}
+.main {
+  background-color: #e9eef3;
+  height: 100%;
+}
 </style>
--- src/components/login/login.vue
+++ src/components/login/login.vue
@@ -1,14 +1,18 @@
 <template>
-	<div>
-		<el-form label-width="80px" :model="userForm">
+	<div class="login-warp">
+		<el-form label-width="80px" :model="userForm" label-position="top">
+			<h4>用户登陆</h4>
 			<el-form-item label="用户名">
 				<el-input type="text" v-model="userForm.username"></el-input>
 			</el-form-item>
 			<el-form-item label="密码">
 				<el-input type="password" v-model="userForm.password"></el-input>
 			</el-form-item>
+		<el-form-item>
+			<el-button type="primary" class="logn-button" @click='login'>立即登陆</el-button>
+			<!-- <el-button type="primary">取消</el-button> -->
+		</el-form-item>
 		</el-form>
-	<el-button type="primary" @click='login'>登陆</el-button>
 	</div>
 </template>
 <script>
@@ -21,7 +25,7 @@ export default {
         password: ""
       }
     };
-	},
+  },
   methods: {
     async login() {
       //  1、准备数据
@@ -39,16 +43,31 @@ export default {
       // console.log(res) 测试结果完成
       // console.log(res.data.meta)
       if (res.data.meta.status === 200) {
-				console.log(11)
         this.$router.push({
           //编程式导航
           name: "home"
         });
       }
     }
-  },
+  }
 };
 </script>
 
-<style scoped>
+<style>
+.login-warp {
+  height: 100%;
+  background: #324152;
+  display: flex;
+  justify-content: center;
+	align-items: center;
+
+}
+.login-warp form{
+		background:darkkhaki;
+		padding: 30px;
+		border-radius: 15px;
+}
+.logn-button{
+	width:100%;
+}
 </style>
